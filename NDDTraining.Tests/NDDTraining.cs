b21d715b431a49ff9f31dce90df53aced1b63ad3
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-11-14 19:28:55 -0300

    Initial

---
+++ NDDTraining.Tests/NDDTraining.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class NDDTraining
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

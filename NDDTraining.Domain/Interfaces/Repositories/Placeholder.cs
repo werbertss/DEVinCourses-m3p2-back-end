cf9646f30fec66491f3cb9d6542c4dbba19a7b09
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2022-11-15 14:38:42 -0300

    Cria a pasta Services

---
+++ NDDTraining.Domain/Interfaces/Repositories/Placeholder.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NDDTraining.Domain.Interfaces.Repositories
+{
+    public interface Placeholder
+    {
+        
+    }
+}

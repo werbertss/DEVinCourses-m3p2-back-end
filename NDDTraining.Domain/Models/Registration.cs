83f82a8cac079d1f9370ce43e7060eaf9a3489a9
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-11-15 16:13:45 -0300

    feat criacao da classe registration e DTO

---
+++ NDDTraining.Domain/Models/Registration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace NDDTraining.Domain.Models
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Status { get; set; }
+
+        public Registration()
+        {
+        }
+    }
+
+}
+

ed4d561ec3f037a4bd2f0e878d220163a4ab3966
Gabriel F Albuquerque <gfonteles.a@gmail.com>
2022-11-16 15:28:52 -0300

    feat: criacao dos servicos, module, registration e training

---
+++ Src/NDDTraining.Domain/Services/TrainingService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NDDTraining.Domain.Interfaces.Services;
+
+namespace NDDTraining.Domain.Services
+{
+  public class TrainingService : ITrainingService
+  {
+    public void DeleteTraining()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void FinishTrainig()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void GetTrainingFinish()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void GetTrainingProgress()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

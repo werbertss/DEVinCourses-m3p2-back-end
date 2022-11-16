f461a6fedc656567a756b3b493e236a6faea1c7d
get.Friday <gabriel.getdev@gmail.com>
2022-11-16 20:04:12 -0300

    Feat: Cria arquivo BaseRepository

---
+++ Src/NDDTraining.Infra/Data/Repository/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDTraining.Infra.Data.Repository
+{
+    public class BaseRepository <TEntity, TKey> where TEntity : class
+    {
+    }
+}

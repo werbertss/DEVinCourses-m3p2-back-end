25f8fb851e5dd5dd66f9ab593e33346f2421e40b
get.Friday <gabriel.getdev@gmail.com>
2022-11-16 20:05:13 -0300

    Feat: Cria arquivo DI

---
+++ Src/NDDTraining.DI/IOC/DI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDTraining.DI.IOC
+{
+    public static class DI
+    {
+    }
+}

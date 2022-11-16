2abe84a95ea2c89fb8cdf3afc3030cfee5376bca
Rodrigo Rosa <rodrigorosa.dev@gmail.com>
2022-11-15 21:06:47 -0300

    feature: create training model and dto

---
+++ Src/NDDTraining.Domain/DTOS/TrainingDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NDDTraining.Domain.Models;
+
+namespace NDDTraining.Domain.DTOS
+{
+	public class TrainingDTO
+	{
+        public int Id { get; internal set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Teacher { get; set; }
+        public TimeSpan Duration { get; set; }
+        public bool Active { get; set; }
+        public string Category { get; set; }
+        // public virtual List<Module> Module { get; set; }
+
+        public TrainingDTO()
+		{
+		}
+        public TrainingDTO(Training training)
+        {
+            Url = training.Url;
+            Title = training.Title;
+            Description = training.Description;
+            Teacher = training.Teacher;
+            Duration = training.Duration;
+            Active = training.Active;
+            Category = training.Category;
+        }
+        public TrainingDTO(TrainingDTO training)
+        {
+            Url = training.Url;
+            Title = training.Title;
+            Description = training.Description;
+            Teacher = training.Teacher;
+            Duration = training.Duration;
+            Active = training.Active;
+            Category = training.Category;
+        }
+    }
+}
+

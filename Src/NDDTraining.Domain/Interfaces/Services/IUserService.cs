f9c1f03c2f6cd4892732d120f4157250f351f837
get.Friday <gabriel.getdev@gmail.com>
2022-11-16 20:04:33 -0300

    Feat: Cria arquivo IUserService

---
+++ Src/NDDTraining.Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDTraining.Domain.Interfaces.Services
+{
+    public interface IUserService
+    {
+    }
+}

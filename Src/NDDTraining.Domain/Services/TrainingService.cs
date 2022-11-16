e2cc686d400e34ce10d8a5e5f2e30ce9e56758e5
Edmilson Gomes <edmilsondmx@gmail.com>
2022-11-16 15:47:16 -0300

    fix: implementa retorno da service training

--- Src/NDDTraining.Domain/Services/TrainingService.cs
+++ Src/NDDTraining.Domain/Services/TrainingService.cs
@@ -2,30 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NDDTraining.Domain.DTOS;
 using NDDTraining.Domain.Interfaces.Services;
 
 namespace NDDTraining.Domain.Services
 {
-  public class TrainingService : ITrainingService
-  {
-    public void DeleteTraining()
+    public class TrainingService : ITrainingService
     {
-      throw new NotImplementedException();
-    }
+        public void DeleteTraining()
+        {
+            throw new NotImplementedException();
+        }
 
-    public void FinishTrainig()
-    {
-      throw new NotImplementedException();
-    }
+        public void FinishTrainig()
+        {
+            throw new NotImplementedException();
+        }
 
-    public void GetTrainingFinish()
-    {
-      throw new NotImplementedException();
-    }
+        public IList<TrainingDTO> GetAll(string category)
+        {
+            throw new NotImplementedException();
+        }
 
-    public void GetTrainingProgress()
-    {
-      throw new NotImplementedException();
+        public TrainingDTO GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TrainingDTO GetTrainingFinish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TrainingDTO GetTrainingProgress()
+        {
+            throw new NotImplementedException();
+        }
     }
-  }
 }

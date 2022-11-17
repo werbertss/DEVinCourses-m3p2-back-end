845bb0d03b0e0b6cf3420d0b36306252b2b679ed
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-11-17 07:15:02 -0300

    feature:Criar GetByTraining IModService e services

--- Src/NDDTraining.Domain/Services/ModuleService.cs
+++ Src/NDDTraining.Domain/Services/ModuleService.cs
@@ -2,20 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NDDTraining.Domain.DTOS;
+using NDDTraining.Domain.Interfaces.Repositories;
 using NDDTraining.Domain.Interfaces.Services;
 
 namespace NDDTraining.Domain.Services
 {
-  public class ModuleService : IModuleService
-  {
-    public void FinishClassrom()
+    public class ModuleService : IModuleService
     {
-      throw new NotImplementedException();
-    }
+        private readonly IModuleRepository _moduleRepository;
 
-    public void GetModule()
-    {
-      throw new NotImplementedException();
+        public ModuleService(IModuleRepository moduleRepository)
+        {
+            _moduleRepository = moduleRepository;
+        }
+
+        public void FinishClassrom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetModule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ModuleDTO> GetByTraining(int trainingId)
+        {
+            return _moduleRepository
+                .GetByTraining(trainingId)
+                .Select(x => new ModuleDTO(x))
+                .ToList();
+        }
+
+        List<ModuleDTO> IModuleService.GetModule()
+        {
+            throw new NotImplementedException();
+        }
     }
-  }
 }

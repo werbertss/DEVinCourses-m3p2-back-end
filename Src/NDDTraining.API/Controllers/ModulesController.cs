1bef5c221bc26edfac37cd3761d0398cac8e8ec1
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-11-17 07:33:47 -0300

    feature: Criar GetByTraining ModulesController

--- Src/NDDTraining.API/Controllers/ModulesController.cs
+++ Src/NDDTraining.API/Controllers/ModulesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NDDTraining.Domain.Interfaces.Services;
+
 
 namespace NDDTraining.API.Controllers
 {
@@ -10,6 +12,25 @@ namespace NDDTraining.API.Controllers
     [Route("api/[controller]")]
     public class ModulesController : ControllerBase
     {
-        
+        private readonly IModuleService _moduleService;
+
+        public ModulesController(IModuleService moduleService)
+        {
+            _moduleService = moduleService;
+        }
+
+        [HttpGet("~trainings/{id}/modules")]
+        public IActionResult GetByTrainin(
+            [FromRoute] int id)
+        {
+            try
+            {
+                return Ok(_moduleService.GetByTraining(id));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
 }

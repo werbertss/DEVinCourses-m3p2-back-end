0fabde609e14705147c66c6566962ab2c3a9fbb4
get.Friday <gabriel.getdev@gmail.com>
2022-11-18 18:01:06 -0300

    Feat: Cria NoDataException

---
+++ Src/NDDTraining.Domain/Exceptions/NoDataException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDTraining.Domain.Exceptions
+{
+    public class NoDataException : Exception
+    {
+        public NoDataException(string message) : base(message)
+        {
+        }
+    }
+}

3d416546c2bd1672c12ca302c3d8bbe6b5c4288c
get.Friday <gabriel.getdev@gmail.com>
2022-11-17 21:51:25 -0300

    Feat: Implementa ctor em Login para LoginDTO

--- Src/NDDTraining.Domain/Models/Login.cs
+++ Src/NDDTraining.Domain/Models/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NDDTraining.Domain.DTOS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,10 @@ namespace NDDTraining.Domain.Models
             Email = email;
             Password = password;
         }
-
+        public Login(LoginDTO loginDTO)
+        {
+            Email = loginDTO.Email;
+            Password = loginDTO.Password;
+        }
     }
 }

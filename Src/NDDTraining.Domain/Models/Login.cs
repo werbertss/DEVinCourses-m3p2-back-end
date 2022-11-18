f9f255fb166f7aa42a4bb2ae6f3810a119611937
get.Friday <gabriel.getdev@gmail.com>
2022-11-17 21:19:13 -0300

    Feat: Implementa login model

---
+++ Src/NDDTraining.Domain/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDTraining.Domain.Models
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Login(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+    }
+}

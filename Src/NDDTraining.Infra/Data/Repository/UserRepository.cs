bf6ad6c8b1c2f0fe988643fe442a81b7fdb99f51
get.Friday <gabriel.getdev@gmail.com>
2022-11-17 21:50:17 -0300

    Feat: Implementa VerifyLogin em UserRepository

--- Src/NDDTraining.Infra/Data/Repository/UserRepository.cs
+++ Src/NDDTraining.Infra/Data/Repository/UserRepository.cs
@@ -1,10 +1,16 @@
-﻿using NDDTraining.Domain.Models;
+﻿using NDDTraining.Domain.Interfaces.Repositories;
+using NDDTraining.Domain.Models;
 using NDDTraining.Infra.Data.Context;
 
 namespace NDDTraining.Infra.Data.Repository
 {
-    public class UserRepository : BaseRepository<User, int>
+    public class UserRepository : BaseRepository<User, int>, IUserRepository
     {
         public UserRepository(NDDTrainingDbContext context) : base(context) { }
+
+        public bool VerifyLogin(Login login)
+        {
+            return _context.Users.Any(u => u.Email == login.Email && u.Password == login.Password);
+        }
     }
 }

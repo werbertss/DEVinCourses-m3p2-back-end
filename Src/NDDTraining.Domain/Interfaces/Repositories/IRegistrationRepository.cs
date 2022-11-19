6e566c47dd697fe018e7f26c9ac4da5a885e3c1d
Analista Testes Junior de Jesus Silva <lucasdjs2014@hotmail.com>
2022-11-19 18:16:43 -0300

    Fix: Exclusão de método sem relevancia

--- Src/NDDTraining.Domain/Interfaces/Repositories/IRegistrationRepository.cs
+++ Src/NDDTraining.Domain/Interfaces/Repositories/IRegistrationRepository.cs
@@ -6,12 +6,9 @@ namespace NDDTraining.Domain.Interfaces.Repositories
 
     public interface IRegistrationRepository : IBaseRepository<Registration, int>
     {
-        IList<Registration>GetAll();
-        void InsertListProgress(RegistrationDTO register );
+        IList<Registration> GetAll();
         public void Delete(int id);
-        void InsertListAvailable(RegistrationDTO register );
-        void InsertListFinished(RegistrationDTO register );
-        void InsertListSuspended(RegistrationDTO register );
+
         bool RegistrationDuplicate(int id);
         public bool DeleteNoRegistration(int id);
         void DeleteRegistration(Registration registration);

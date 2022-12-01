e322e14693f3f975a43f79981335b07657f4f3ad
Webert <webert_silva@estudante.sesisenai.org.br>
2022-12-01 09:57:38 -0300

    Inserindo teste Training

---
+++ Src/NDDTrainig.Test/TrainingsTestValidations/TrainingTestUnit.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NDDTraining.Domain.DTOS;
+using NDDTraining.Domain.Validations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDTrainig.Test.TrainingsTestValidations
+{
+    [TestFixture]
+    public class TrainingTestUnit
+    {
+        private TrainingValidation validador;
+        [SetUp]
+        public void Setup()
+        {
+            validador = new TrainingValidation();
+        }
+
+        [TestCase(456)]
+        public void Validate_CampoPreenchido()
+        {
+            var result = new TrainingDTO { Id = 50 };
+            Assert.IsTrue(result != null);
+        }
+    }
+
+}
---
+++ Src/NDDTraining.Domain/Validations/TrainingValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using NDDTraining.Domain.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDTraining.Domain.Validations
+{
+    public class TrainingValidation : AbstractValidator<TrainingDTO>
+    {
+        public TrainingValidation() 
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Campo deve estar preenchido");
+        }
+
+
+    }
+}
